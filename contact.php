035bb5f38199f38d88d7f604562a3d6d61975aea
Jayant Verma <jayantverma8533@gmail.com>
2022-04-04 13:10:53 +0530

    First commit

---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php
+// variable setting
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$subject = $_REQUEST['subject'];
+$message = $_REQUEST['message'];
+
+mail("jayantverma8533@gmail.com", $subject, $message, "From: $name <$email>")
